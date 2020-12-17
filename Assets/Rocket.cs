fc941c8d75ff084bc8d100f1895bfaeb7a068601
Melacsirrah <melacsirrah575@gmail.com>
2020-12-16 20:04:49 -0400

    Houston, the rocket now flies

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+   Rigidbody rigidBody;
+
+//These values are the base values. Can be changed in Unity
+   [SerializeField] float rcsThrust = 100f;
+   [SerializeField] float mainThrust = 100f;
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>(); 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        RespondToRotateInput();
+        RespondToThrustInput();
+    }
+
+    private void RespondToRotateInput()
+    {
+        //This removes the rotation due to physics
+        rigidBody.angularVelocity = Vector3.zero;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+    }
+
+    private void RespondToThrustInput()
+    {
+        float thrustThisFrame = mainThrust *Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust(thrustThisFrame);
+        }
+        else
+        {
+            StopApplyingThrust();
+        }
+    }
+
+    private void StopApplyingThrust()
+    {
+        //This is where audio and particle effects would be told to stop
+    }
+
+    private void ApplyThrust(float thrustThisFrame)
+    {
+        rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+
+        //Will tell audio/particle effects to start here if given enough time
     }
 }
